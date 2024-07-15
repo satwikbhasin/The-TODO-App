8bf43a3bba0211ce779566ebfc40c3c807ef8453
Satwik Bhasin <satwikbhasin@gmail.com>
2024-07-15 00:45:11 -0400

    updated settings.py to allow s3 frontend

--- backend/todoproject/settings.py
+++ backend/todoproject/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://roulettech-todo-app-frontend.s3-website-us-east-1.amazonaws.com"
 ]
 
 ROOT_URLCONF = 'todoproject.urls'
